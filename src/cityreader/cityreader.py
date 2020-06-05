04325b253918e156752b9a97fc853f96f1f78b28
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-06-05 13:47:49 -0400

    cityreader updated with float

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -17,10 +17,13 @@ import csv
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 class City:
-  def __init__(self,name, lat, lon):
+  def __init__(self, name, lat, lon):
       self.name = name
       self.lat = lat
       self.lon = lon
+
+  def __str__(self):
+    return f'{self.name}, {self.lat}, {self.lon}'
 
 cities = []
 
@@ -28,6 +31,12 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+    with open('cities.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        for row in reader:
+            new_city = City(row[0], float(row[3]), float(row[4]))
+            cities.append(new_city)
     
     return cities
 
