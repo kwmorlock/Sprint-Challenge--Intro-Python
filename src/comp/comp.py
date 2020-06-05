0fe4681b14d8b238277433d530bd3a316eec4130
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-06-05 12:25:52 -0400

    age update and uppercase comp

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -63,8 +63,10 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [Human(nu.name.upper(), nu.age+5) for nu in humans]
 print(g)
+#The upper() method returns a string where all characters are in upper case. 
+#Symbols and Numbers are ignored.
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
