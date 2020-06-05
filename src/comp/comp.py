682917866710106667c347027a3cead40385a25b
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-06-05 12:27:43 -0400

    sqrt comp

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -71,5 +71,5 @@ print(g)
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(a.age) for a in humans]
 print(h)
