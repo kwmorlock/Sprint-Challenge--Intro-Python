1c1780ae9148a91a6a39cdbda681f8641dd8223b
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-06-05 12:42:31 -0400

    city init and import

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,7 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
+#https://docs.python.org/3/library/csv.html
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -14,6 +16,12 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+class City:
+  def __init__(self,name, lat, lon):
+      self.name = name
+      self.lat = lat
+      self.lon = lon
+
 cities = []
 
 def cityreader(cities=[]):
